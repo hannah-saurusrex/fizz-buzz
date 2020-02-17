cc391842e742123036f6372edf29ba4bbf8af51d
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-17 13:36:11 -0700

    added hacker rank problems to solve at a later date

--- index.js
+++ index.js
@@ -3,19 +3,16 @@
 // 2. for multiples of 5 print "buzz";
 // 3. for multiples of 3 print "fizz";
 
-function fizzbuzz() {
-    for (var i = 1; i <=100; i++) {
-        if ( i % 15 === 0) {
-            console.log("Fizzbuzz");
-        } else if (i % 5 === 0) {
-            console.log("buzz");
-        } else if (i % 3 ===0) {
-            console.log("fizz");
-        } else {
-            console.log(i);
-        }
+for(var i = 1; i < 101; i++) {
+    var multipleOfThree = i % 3 === 0;
+    var multipleOfFive = i % 5 === 0;
+    if (multipleOfThree && multipleOfFive) {
+        console.log("fizzbuzz");
+    } else if(i % 3 === 0) {
+        console.log("fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
     }
 }
-
-fizzbuzz();
-
